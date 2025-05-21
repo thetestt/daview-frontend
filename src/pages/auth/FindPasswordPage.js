57ec3ea29e96b1aea7b9463a8b1c2bafb17222fa
조수현 <makecritical01@gmail.com>
2025-05-21 17:48:05 +0900

    로그인 페이지 수정 및 아이디찾기 비밀번호찾기 화면 구성

---
+++ src/pages/auth/FindPasswordPage.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "../../styles/auth/FindPasswordPage.css";
+import Footer from "../../components/Footer";
+
+function FindPasswordPage() {
+  return <div>비밀번호찾기</div>;
+}
+
+export default FindPasswordPage;
