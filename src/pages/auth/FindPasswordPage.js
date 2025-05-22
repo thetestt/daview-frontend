233a1c66712f844c7a1c1d0fac9685c1ba9db69c
조수현 <makecritical01@gmail.com>
2025-05-22 10:00:27 +0900

    코드 수정

--- src/pages/auth/FindPasswordPage.js
+++ src/pages/auth/FindPasswordPage.js
@@ -1,6 +1,5 @@
 import React from "react";
 import "../../styles/auth/FindPasswordPage.css";
-import Footer from "../../components/Footer";
 
 function FindPasswordPage() {
   return <div>비밀번호찾기</div>;
