3e1a2af6bd3b69b6baacc7a5ce853bc68d854ed2
SuanChoi-Claire <clairechoi.h@gmail.com>
2025-06-05 15:59:05 +0900

    검색박스 지역, 시군구 테이블 따로생성 후 DB 연동

--- src/api/SearchResults.js
+++ src/api/SearchResults.js
@@ -12,3 +12,15 @@ export const fetchSearchResults = async (query) => {
     throw error;
   }
 };
+
+//서치 박스에서 지역을 선택하면 시구군을 바꿔주는 API
+export const getCityListByRegion = async (regionId) => {
+  const response = await axios.get(`/api/region/${regionId}/cities`);
+  return response.data;
+};
+
+//지역 가져오는 API
+export const getRegionList = async () => {
+  const response = await axios.get("/api/region");
+  return response.data;
+};
