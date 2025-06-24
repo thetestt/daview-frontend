32b5469dfca9661e6b1f42b2a6d74e59fa8032c1
kimi <jikimi93@gmail.com>
2025-06-24 17:32:48 +0900

    CSS Module 적용 (main, header, footer)

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import logoImg from "../assets/daview-logo.png";
-import "../styles/components/Header.css";
+import styles from "../styles/components/Header.module.css";
 import { jwtDecode } from "jwt-decode";
 
 function Header() {
@@ -62,38 +62,40 @@ function Header() {
 
   return (
     <div>
-      <header className="header">
+      <header className={styles["header"]}>
         {/* 왼쪽: 로고 + 텍스트 */}
-        <div className="header-left">
-          <div className="logo-container">
-            <Link to="/" className="logo">
-              <img src={logoImg} alt="LOGO" className="logo-image" />
+        <div className={styles["header-left"]}>
+          <div className={styles["logo-container"]}>
+            <Link to="/" className={styles["logo"]}>
+              <img src={logoImg} alt="LOGO" className={styles["logo-image"]} />
             </Link>
-            <span className="logo-text">전국 모든 요양원 / 실버타운</span>
+            <span className={styles["logo-text"]}>
+              전국 모든 요양원 / 실버타운
+            </span>
           </div>
         </div>
 
         {/* 가운데: 검색 input + 버튼 */}
-        <div className="header-right">
-          <div className="search-box">
-            <span className="search-icon">
+        <div className={styles["header-right"]}>
+          <div className={styles["search-box"]}>
+            <span className={styles["search-icon"]}>
               <img src="/images/buttonimage/searchicon.png" alt="검색" />
             </span>
             <input
               type="text"
-              className="search-input"
+              className={styles["search-input"]}
               placeholder="검색어를 입력하세요"
               value={keyword}
               onChange={(e) => setKeyword(e.target.value)}
             />
-            <button className="search-button" onClick={handleSearch}>
+            <button className={styles["search-button"]} onClick={handleSearch}>
               검색
             </button>
           </div>
 
           {/* 오른쪽: 로그인 / 회원가입 */}
           {username ? (
-            <div className="user-actions">
+            <div className={styles["user-actions"]}>
               <span>{username}님</span>
               <Link to="/mypage">마이페이지</Link>
               <Link to={`/reservation/member/${memberId}`}>나의예약</Link>
@@ -109,42 +111,42 @@ function Header() {
       </header>
 
       {/* 네비게이션 */}
-      <nav className="nav-bar">
+      <nav className={styles["nav-bar"]}>
         <ul>
-          <li className="dropdown">
+          <li className={styles["dropdown"]}>
             <a href="/caregiver">요양사</a>
-            <ul className="dropdown-menu">
+            <ul className={styles["dropdown-menu"]}>
               <li>
                 <a href="/nursinghome">상품 보러가기</a>
                 <a href="/nursinghome">예약하기</a>
               </li>
             </ul>
           </li>
-          <li className="dropdown">
+          <li className={styles["dropdown"]}>
             <a href="/nursinghome">요양원</a>
-            <ul className="dropdown-menu">
+            <ul className={styles["dropdown-menu"]}>
               <li>
                 <a href="/nursinghome">상품 보러가기</a>
                 <a href="/nursinghome">예약하기</a>
               </li>
             </ul>
           </li>
-          <li className="dropdown">
+          <li className={styles["dropdown"]}>
             <a href="/silvertown">실버타운</a>
-            <ul className="dropdown-menu">
+            <ul className={styles["dropdown-menu"]}>
               <li>
                 <a href="/nursinghome">상품 보러가기</a>
                 <a href="/nursinghome">예약하기</a>
               </li>
             </ul>
           </li>
-          <li className="dropdown">
+          <li className={styles["dropdown"]}>
             <a href="#">고객의 소리</a>
-            <ul className="dropdown-menu">
+            <ul className={styles["dropdown-menu"]}>
               <li>
-                <a href="#">공지게시판</a>
-                <a href="#">문의하기</a>
-                <a href="#">고객 후기</a>
+                <Link to="#">공지게시판</Link>
+                <Link to="/review-board">문의하기</Link>
+                <Link to="#">고객 후기</Link>
               </li>
             </ul>
           </li>
